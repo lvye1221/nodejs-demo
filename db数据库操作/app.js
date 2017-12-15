e02f9fd351abaf4c8d6459401ed19f944439070b
杨杰 <616651001@qq.com>
2017-12-15 15:15:16 +0800

    采用 express 4.0 更新项目

--- db数据库操作/app.js
+++ db数据库操作/app.js
@@ -1,13 +1,46 @@
-var express = require('express')
-// , routes = require('./routes')
-// , user = require('./routes/user')
-, movie = require('./routes/movie')
-, http = require('http')
-, path = require('path')
-// , ejs = require('ejs')
-, SessionStore = require("session-mongoose")(express);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.get('/movie/add',movie.movieAdd);//增加
-app.post('/movie/add',movie.doMovieAdd);//提交
-app.get('/movie/:name',movie.movieAdd);//编辑查询
-app.get('/movie/json/:name',movie.movieJSON);//JSON数据
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
